e13226ae8be31a370a02326d0c15167bda4c9d13
wisnoskij <jonathonwisnoski@gmail.com>
2022-11-16 15:42:33 -0500

    Added Cli argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,76 @@
+#![allow(unused_parens)]
+#![allow(unused_imports)] //TODO: remove
+#![allow(dead_code)] //TODO: remove
+
+//CLi argument management
 use clap::Parser;
-use std::path::Path;
+
+use std::path::PathBuf;
+
+//file management
+use tempfile::tempfile;
+use std::io::{self, Write};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-	/// Folder to convert
-	#[arg(short = None, long = None, value_name = "FOLDER",default_value_t = Path::new("."))]
-	folder: Path,
+	/// Folder to convert [TODO: if file instead of folder]
+	#[arg(short = None, long = None, value_name = "FOLDER/FILE", value_parser = parse_path, default_value = ".")]
+	path: PathBuf,
 
-	/// Start processing at FILE.txt alpha numerically (inclusive).
+	/// Start processing at FILE.txt alpha numerically (inclusive). [TODO]
 	#[arg(short, long, value_name = "FILE")]
-	from: &'static str,
+	from: Option<String>,
 
-	/// Stop processing at FILE.txt alpha numerically (inclusive).
+	/// Stop processing at FILE.txt alpha numerically (inclusive). [TODO]
 	#[arg(short, long, value_name = "FILE")]
-	to: &'static str,
+	to: Option<String>,
+
+	/// Overwrite existing mp3 files instead of skipping [TODO]
+	#[arg(short, long)]
+	overwrite: bool,
+
+	/// Recursively go into directories. [TODO]
+	#[arg(short, long)]
+	recurse: bool,
+
+	/// Shutdown system after finishing. [TODO]
+	#[arg(short, long)]
+	shutdown: bool,
+
+	/// Remove non alphanumeric characters and normal punctuation. [TODO]
+	#[arg(short, long)]
+	normalize: bool,
+
+	/// Fix troublesome abbreviations. [TODO]
+	#[arg(short, long)]
+	abbreviations: bool,
 }
 
-// single file mode
-// lvl/LVL to level
-// recursive down dirs
+// Parse the path into a Path, verify it exists
+fn parse_path(p: &str) -> Result<PathBuf, String> {
+	let path: PathBuf = PathBuf::from(p);
+	if(path.exists()) {
+		return(Ok(path) as Result<PathBuf, String>);
+	}
+	return(Err(format!("Supplied path ({}) does not exist", path.display())) as Result<PathBuf, String>);
+}
+
+const IN_EXT: &str = ".txt";
+const OUT_EXT: &str = ".mp3";
+const FIXED_EXT: &str = ".gtts-tmp"; //TODO
+
+// lvl/LVL to level '=>"
+// sd 'lvl' 'level' *.txt; sd 'LVL' 'level' *.txt; sd ' ' '' *.txt; sd '』' '' *.txt; sd '『' '' *.txt; sd '°' '' *.txt; sd ' Lv' ' level ' *.txt; sd ' lv' ' level ' *.txt
+
+//able to press key to stop at next finished file
 
 fn main() {
 	let args: Args = Args::parse();
+
+	println!("{}", args.path.display());//TODO: remove
+	println!("is DIR:{}", args.path.is_dir());//TODO: remove
+	println!("is File:{}", args.path.is_file());//TODO: remove
+
+	
 }
